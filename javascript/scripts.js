6c41df990e5a13c5b0d07b087256bb5a41f2d6c5
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2020-07-31 14:41:43 +0200

    Fix hover transparency. Change template nav item

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('#head').hover(function () {
-        $('#menu').show("slide", {direction: "left" }, "slow");
-        }, function () {
-        $('#menu').hide("slide", {direction: "left" }, "slow");
-    });
+    // $('#head').hover(function () {
+    //     $('#menu').show("slide", {direction: "left" }, "slow");
+    //     }, function () {
+    //     $('#menu').hide("slide", {direction: "left" }, "slow");
+    // });
 
     $('[data-fancybox]').fancybox({
         protect: true
